4858343c1383ca8ee5b1cb68cdc231b447485bb0
unknown <eskohai@AOTA11LDML-17D.kamit.fi>
2015-12-16 12:48:52 +0200

    changed to 2013

--- MultiPong/include/Messages.h
+++ MultiPong/include/Messages.h
@@ -1,30 +1,30 @@
-#pragma once
-#ifndef MESSAGES_H
-#define MESSAGES_H
-
-#include "util.h"
-
-template <class T>
-class Message
-{
-public:
-	virtual char* Create() = 0;
-	virtual T Receive(char *) = 0;
-};
-
-class TimeMessage : Message<TimeMessage>
-{
-public:
-	TimeMessage();
-	TimeMessage(int Time);
-	~TimeMessage();
-
-	const uint32_t type = 0;
-	int time;
-
-	// Inherited via Message
-	virtual char * Create() override;
-	virtual TimeMessage Receive(char *) override;
-};
-
-#endif
+//#pragma once
+//#ifndef MESSAGES_H
+//#define MESSAGES_H
+//
+//#include "util.h"
+//
+//template <class T>
+//class Message
+//{
+//public:
+//	virtual char* Create() = 0;
+//	virtual T Receive(char *) = 0;
+//};
+//
+//class TimeMessage : Message<TimeMessage>
+//{
+//public:
+//	TimeMessage();
+//	TimeMessage(int Time);
+//	~TimeMessage();
+//
+//	const uint32_t type = 0;
+//	int time;
+//
+//	// Inherited via Message
+//	virtual char * Create() override;
+//	virtual TimeMessage Receive(char *) override;
+//};
+//
+//#endif
--- MultiPong/src/Messages.cpp
+++ MultiPong/src/Messages.cpp
@@ -1,28 +1,28 @@
-#include "Messages.h"
-
-
-
-TimeMessage::TimeMessage()
-	: type(0)
-{
-}
-
-TimeMessage::TimeMessage(int Time)
-	: time(Time)
-{
-}
-
-
-TimeMessage::~TimeMessage()
-{
-}
-
-char * TimeMessage::Create()
-{
-	return nullptr;
-}
-
-TimeMessage TimeMessage::Receive(char *)
-{
-	return TimeMessage();
-}
+//#include "Messages.h"
+//
+//
+//
+//TimeMessage::TimeMessage()
+//	: type(0)
+//{
+//}
+//
+//TimeMessage::TimeMessage(int Time)
+//	: time(Time)
+//{
+//}
+//
+//
+//TimeMessage::~TimeMessage()
+//{
+//}
+//
+//char * TimeMessage::Create()
+//{
+//	return nullptr;
+//}
+//
+//TimeMessage TimeMessage::Receive(char *)
+//{
+//	return TimeMessage();
+//}
